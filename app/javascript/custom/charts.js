baabbefe7d5d316c5c2fe7f16d26e2210dc508f8
Prometheus <berjistechnologies@gmail.com>
2022-03-19 02:29:19 +0300

    Chart basics

--- app/javascript/custom/charts.js
+++ app/javascript/custom/charts.js
@@ -1,3 +1,19 @@
+import Chart from 'chart.js/auto'
+
 document.addEventListener("DOMContentLoaded", function (event) {
-    
+    document.addEventListener('turbolinks:load', () => {
+        let ctx = document.getElementById('chart_pane').getContext('2d')
+
+        if (window.location.href == `${base_url}dashboard`) {
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: JSON.parse(ctx.canvas.dataset.labels),
+                    datasets: [{
+                        data: JSON.parse(ctx.canvas.dataset.data),
+                    }]
+                },
+            })
+        }
+    })
 })
