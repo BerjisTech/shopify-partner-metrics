6f4f8e2180359a371de49109068b4632a5f9b63d
prometheus <berjistechnologies@gmail.com>
2022-04-30 18:20:35 +0300

    external_metric#recet() fix (still not working)

--- app/models/shopify_import.rb
+++ app/models/shopify_import.rb
@@ -6,23 +6,22 @@ class ShopifyImport < ApplicationRecord
   class << self
     def start_importer(app_id, api, time, data_set, cursor = '')
       ImportLog.create!({ platform_id: time[:start], app_id: app_id, start_time: DateTime.now, status: 0 })
-      ExternalMetric.where(app_id: app_id, date: time[:end].to_date.strftime('%d-%m-%Y'),
-                           platform_id: PLATFORM).dstroy_all
+
       run_data(app_id, api, time, data_set, cursor)
     end
 
     def run_data(app_id, api, time, data_set, cursor = '')
       data = pick_importer(api, data_set, time, cursor)
 
-      if data.present?
-        data = JSON.parse(data)
+      return Rollbar.error(data) unless data.present?
 
-        records = OpenStruct.new data['data']
+      data = JSON.parse(data)
 
-        process(app_id, api, time, records, data_set, cursor)
-      else
-        Rollbar.error(data)
-      end
+      records = OpenStruct.new data['data']
+
+      return Rollbar.error(records) unless records.present?
+
+      process(app_id, api, time, records, data_set, cursor) if records.present?
     end
 
     def pick_importer(api, data_set, time, cursor)
@@ -32,8 +31,6 @@ class ShopifyImport < ApplicationRecord
         data_importer(api, ShopifyPayment.gql_data(time, cursor))
       end
     end
-
-    def check_blank(data, app, time); end
 
     def data_importer(api, body)
       path = "https://partners.shopify.com/#{api.partner_id}/api/2021-10/graphql.json"
@@ -45,35 +42,37 @@ class ShopifyImport < ApplicationRecord
     end
 
     def process(app_id, api, time, records, data_set, cursor)
+
+      return Rollbar.error(records) unless records['app'].present? || records['transactions'].present?
+
       results = if data_set == 'user'
-                  records.app['events']
+                  records['app']['events']
                 else
-                  records.transactions
+                  records['transactions']
                 end
-      if edges.present? && edges.size.positive?
-        edges = results['edges']
 
-        if data_set.include? 'test'
-          edges.group_by { |e| e['node']['app']['name'] }
-        else
-          final_data_set = if data_set == 'user'
-                             ShopifyUser.process_data(edges, app_id, time[:end], PLATFORM, cursor)
-                           else
-                             grouped = edges.filter { |e| e['node']['app']['name'] == App.find(app_id).app_name }
-                             ShopifyPayment.process_data(grouped, app_id, time[:end], PLATFORM, cursor, data_set)
-                           end
+      return Rollbar.error(results) unless results.present? && results.size.positive?
 
-          if results['pageInfo']['hasNextPage']
-            run_data(app_id, api, time, data_set,
-                     edges.last['cursor'])
-          else
-            ImportLog.where(platform_id: PLATFORM, app_id: app_id).update_all({ status: 1, end_time: DateTime.now })
-          end
+      edges = results['edges']
 
-          ExternalMetric.where(app_id: app_id, platform_id: PLATFORM, date: time[:end].to_date.strftime('%d-%m-%Y'))
-        end
+      if data_set.include? 'test'
+        edges.group_by { |e| e['node']['app']['name'] }
       else
-        Rollbar.error(edges)
+        final_data_set = if data_set == 'user'
+                           ShopifyUser.process_data(edges, app_id, time[:end], PLATFORM, cursor)
+                         else
+                           grouped = edges.filter { |e| e['node']['app']['name'] == App.find(app_id).app_name }
+                           ShopifyPayment.process_data(grouped, app_id, time[:end], PLATFORM, cursor, data_set)
+                         end
+
+        if results['pageInfo']['hasNextPage']
+          run_data(app_id, api, time, data_set,
+                   edges.last['cursor'])
+        else
+          ImportLog.where(platform_id: PLATFORM, app_id: app_id).update_all({ status: 1, end_time: DateTime.now })
+        end
+
+        ExternalMetric.where(app_id: app_id, platform_id: PLATFORM, date: time[:end].to_date.strftime('%d-%m-%Y'))
       end
     end
   end
