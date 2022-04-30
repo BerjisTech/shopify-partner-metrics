3570c7d9dd3172601dfec8a5d0e77881fb2d8637
prometheus <berjistechnologies@gmail.com>
2022-04-30 17:47:47 +0300

    Add Rollbar to Shopify import

--- app/models/shopify_import.rb
+++ app/models/shopify_import.rb
@@ -6,18 +6,23 @@ class ShopifyImport < ApplicationRecord
   class << self
     def start_importer(app_id, api, time, data_set, cursor = '')
       ImportLog.create!({ platform_id: time[:start], app_id: app_id, start_time: DateTime.now, status: 0 })
-      ExternalMetric.where(app_id: app_id, date: time[:end].to_date.strftime('%d-%m-%Y'), platform_id: PLATFORM).dstroy_all
+      ExternalMetric.where(app_id: app_id, date: time[:end].to_date.strftime('%d-%m-%Y'),
+                           platform_id: PLATFORM).dstroy_all
       run_data(app_id, api, time, data_set, cursor)
     end
 
     def run_data(app_id, api, time, data_set, cursor = '')
       data = pick_importer(api, data_set, time, cursor)
 
-      data = JSON.parse(data)
+      if data.present?
+        data = JSON.parse(data)
 
-      records = OpenStruct.new data['data']
+        records = OpenStruct.new data['data']
 
-      process(app_id, api, time, records, data_set, cursor)
+        process(app_id, api, time, records, data_set, cursor)
+      else
+        Rollbar.error(data)
+      end
     end
 
     def pick_importer(api, data_set, time, cursor)
@@ -45,26 +50,30 @@ class ShopifyImport < ApplicationRecord
                 else
                   records.transactions
                 end
-      edges = results['edges']
+      if edges.present? && edges.size.positive?
+        edges = results['edges']
 
-      if data_set.include? 'test'
-        edges.group_by { |e| e['node']['app']['name'] }
-      else
-        final_data_set = if data_set == 'user'
-                           ShopifyUser.process_data(edges, app_id, time[:end], PLATFORM, cursor)
-                         else
-                           grouped = edges.filter { |e| e['node']['app']['name'] == App.find(app_id).app_name }
-                           ShopifyPayment.process_data(grouped, app_id, time[:end], PLATFORM, cursor, data_set)
-                         end
-
-        if results['pageInfo']['hasNextPage']
-          run_data(app_id, api, time, data_set,
-                   edges.last['cursor'])
+        if data_set.include? 'test'
+          edges.group_by { |e| e['node']['app']['name'] }
         else
-          ImportLog.where(platform_id: PLATFORM, app_id: app_id).update_all({ status: 1, end_time: DateTime.now })
-        end
+          final_data_set = if data_set == 'user'
+                             ShopifyUser.process_data(edges, app_id, time[:end], PLATFORM, cursor)
+                           else
+                             grouped = edges.filter { |e| e['node']['app']['name'] == App.find(app_id).app_name }
+                             ShopifyPayment.process_data(grouped, app_id, time[:end], PLATFORM, cursor, data_set)
+                           end
 
-        ExternalMetric.where(app_id: app_id, platform_id: PLATFORM, date: time[:end].to_date.strftime('%d-%m-%Y'))
+          if results['pageInfo']['hasNextPage']
+            run_data(app_id, api, time, data_set,
+                     edges.last['cursor'])
+          else
+            ImportLog.where(platform_id: PLATFORM, app_id: app_id).update_all({ status: 1, end_time: DateTime.now })
+          end
+
+          ExternalMetric.where(app_id: app_id, platform_id: PLATFORM, date: time[:end].to_date.strftime('%d-%m-%Y'))
+        end
+      else
+        Rollbar.error(edges)
       end
     end
   end
