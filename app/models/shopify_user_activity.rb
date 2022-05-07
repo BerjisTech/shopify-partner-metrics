bf700e8d5594bb06dc7273445a2a3737973e57eb
prometheus <berjistechnologies@gmail.com>
2022-05-08 00:09:32 +0300

    Save shopify user activity

--- app/models/shopify_user_activity.rb
+++ app/models/shopify_user_activity.rb
@@ -9,9 +9,19 @@ class ShopifyUserActivity < ApplicationRecord
       data.map do |i|
         description = i['node']['description'].present? ? i['node']['description'] : nil
         reason = i['node']['reason'].present? ? i['node']['reason'] : nil
+        activity = case i['node']['type']
+                   when 'RELATIONSHIP_INSTALLED'
+                     'install'
+                   when 'RELATIONSHIP_UNINSTALLED'
+                     'uninstall'
+                   when 'RELATIONSHIP_DEACTIVATED'
+                     'deactivated'
+                   when 'RELATIONSHIP_REACTIVATED'
+                     'reactivated'
+                   end
 
         ShopifyUserActivity.find_or_create_by!(
-          activity: i['node']['type'].gsub('ED', 'd').gsub('RELATIONSHIP_', '').downcase,
+          activity: activity,
           app_id: app_id,
           date: i['node']['occurredAt'].to_date,
           shopify_domain: i['node']['shop']['myshopifyDomain'],
